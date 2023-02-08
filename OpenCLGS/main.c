fd72ccc20a1bef93deee35a156877d0ebe00cadb
Randy J. Spaulding <randys006@gmail.com>
2023-02-08 13:21:03 -0700

    3 types

--- OpenCLGS/main.c
+++ OpenCLGS/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define CL_TARGET_OPENCL_VERSION 300
+
 #ifdef __APPLE__
 #include <OpenCL/opencl.h>
 #else
@@ -9,12 +11,18 @@
 
 #define MAX_SOURCE_SIZE (0x100000)
 
+#define CHECK_CL_ERR(__code) __code; if (ret != CL_SUCCESS) printf("CL error: '%d' on line %u\n", ret, __LINE__);
+
+#define TYPE float
+#define KERNEL "vector_add_float"
+#define DEVICE 0
+
 int main(void) {
     // Create the two input vectors
     int i;
-    const int LIST_SIZE = 102400;
-    int *A = (int*)malloc(sizeof(int)*LIST_SIZE);
-    int *B = (int*)malloc(sizeof(int)*LIST_SIZE);
+    const int LIST_SIZE = 10240000;
+    TYPE *A = (TYPE*)malloc(sizeof(TYPE)*LIST_SIZE);
+    TYPE *B = (TYPE*)malloc(sizeof(TYPE)*LIST_SIZE);
     for(i = 0; i < LIST_SIZE; i++) {
         A[i] = i;
         B[i] = LIST_SIZE - i;
@@ -35,81 +43,110 @@ int main(void) {
     fclose( fp );
 
     // Get platform and device information
+    #define  PARAM_SIZE 128
     cl_platform_id * platform_ids = NULL;
-    cl_device_id device_id = NULL;   
+    cl_device_id** device_ids = NULL;   
     cl_uint ret_num_devices;
     cl_uint ret_num_platforms;
     cl_int ret = clGetPlatformIDs(0, NULL, &ret_num_platforms);
     platform_ids = (cl_platform_id*)malloc(sizeof(cl_platform_id) * ret_num_platforms);
+    device_ids = (cl_device_id**)malloc(sizeof(cl_device_id*) * ret_num_platforms);
+
     ret = clGetPlatformIDs(ret_num_platforms, platform_ids, NULL);
-char vendor_name[128] = {0};
-char platform_name[128] = {0};
-       for (cl_uint ui=0; ui< ret_num_platforms; ++ui)
-        {
-                ret = clGetPlatformInfo(platform_ids[ui],
+char vendor_name[PARAM_SIZE] = {0};
+char platform_name[PARAM_SIZE] = {0};
+    for (cl_uint ui=0; ui< ret_num_platforms; ++ui)
+    {
+        ret = clGetPlatformInfo(platform_ids[ui],
                               CL_PLATFORM_VENDOR, 
-                              128 * sizeof(char), 
+                              PARAM_SIZE * sizeof(char), 
                               vendor_name, 
                               NULL);
-                ret = clGetPlatformInfo(platform_ids[ui],
+        ret = clGetPlatformInfo(platform_ids[ui],
                               CL_PLATFORM_NAME, 
-                              128 * sizeof(char), 
+                              PARAM_SIZE * sizeof(char), 
                               platform_name, 
                               NULL);
-                if (CL_SUCCESS != ret) 
-                {
+        if (CL_SUCCESS != ret) 
+        {
                         // handle error
-                }
-                if (vendor_name != NULL)
-                {
-                        printf("Platform %u: %s by %s\n", ui, platform_name, vendor_name);
-                }
+        }
+        if (vendor_name != NULL)
+        {
+            printf("Platform %u: %s by %s\n", ui, platform_name, vendor_name);
+        }
+
+        ret = clGetDeviceIDs( platform_ids[0], CL_DEVICE_TYPE_ALL, 1, 
+            NULL, &ret_num_devices);
+
+        printf("    %u devices:\n", ret_num_devices);
+        device_ids[ui] = (cl_device_id*)malloc(sizeof(cl_device_id) * ret_num_devices);
+
+        ret = clGetDeviceIDs( platform_ids[0], CL_DEVICE_TYPE_ALL, ret_num_devices, 
+            device_ids[ui], NULL);
+
+        for (cl_uint dev = 0; dev < ret_num_devices; ++dev)
+        {
+            char device_info[PARAM_SIZE] = {0};
+            size_t device_info2 = {0};
+            size_t global_mem_info = {0};
+            CHECK_CL_ERR(ret = clGetDeviceInfo(device_ids[ui][dev], CL_DEVICE_NAME, PARAM_SIZE * sizeof(char), device_info, NULL));
+            CHECK_CL_ERR(ret = clGetDeviceInfo(device_ids[ui][dev], CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(size_t), &device_info2, &global_mem_info));
+        //     CHECK_CL_ERR(ret = clGetDeviceInfo(device_ids[ui][dev], CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(cl_uint), &global_mem_info, global_mem_info));
+            if (ret == CL_SUCCESS)
+            {
+                printf("        Device %u name: %32s having %6lu MiB global mem\n", dev, device_info, device_info2 / 1024 / 1024);
+            }
+        }
     }
 
-    ret = clGetDeviceIDs( platform_ids[0], CL_DEVICE_TYPE_ALL, 1, 
-            &device_id, &ret_num_devices);
+    cl_device_id device_id = device_ids[0][DEVICE];
 
     // Create an OpenCL context
-    cl_context context = clCreateContext( NULL, 1, &device_id, NULL, NULL, &ret);
-
+    CHECK_CL_ERR(cl_context context = clCreateContext( NULL, 1, &device_id, NULL, NULL, &ret));
+printf("Created context\n");
     // Create a command queue
-    cl_command_queue command_queue = clCreateCommandQueue(context, device_id, CL_QUEUE_PROFILING_ENABLE, &ret);
+    const cl_queue_properties properties[] = { CL_QUEUE_PROPERTIES, CL_QUEUE_PROFILING_ENABLE, 0 };
+    CHECK_CL_ERR(cl_command_queue command_queue = clCreateCommandQueueWithProperties(context, device_id, properties, &ret));
 
+printf("Created queue\n");
     // Create memory buffers on the device for each vector 
-    cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, 
-            LIST_SIZE * sizeof(int), NULL, &ret);
-    cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,
-            LIST_SIZE * sizeof(int), NULL, &ret);
-    cl_mem c_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, 
-            LIST_SIZE * sizeof(int), NULL, &ret);
+    CHECK_CL_ERR(cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, 
+            LIST_SIZE * sizeof(TYPE), NULL, &ret));
+    CHECK_CL_ERR(cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,
+            LIST_SIZE * sizeof(TYPE), NULL, &ret));
+    CHECK_CL_ERR(cl_mem c_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, 
+            LIST_SIZE * sizeof(TYPE), NULL, &ret));
 
+printf("Created buffers\n");
     // Copy the lists A and B to their respective memory buffers
-    ret = clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0,
-            LIST_SIZE * sizeof(int), A, 0, NULL, NULL);
-    ret = clEnqueueWriteBuffer(command_queue, b_mem_obj, CL_TRUE, 0, 
-            LIST_SIZE * sizeof(int), B, 0, NULL, NULL);
+    CHECK_CL_ERR(ret = clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0,
+            LIST_SIZE * sizeof(TYPE), A, 0, NULL, NULL));
+    CHECK_CL_ERR(ret = clEnqueueWriteBuffer(command_queue, b_mem_obj, CL_TRUE, 0, 
+            LIST_SIZE * sizeof(TYPE), B, 0, NULL, NULL));
 
     // Create a program from the kernel source
     cl_program program = clCreateProgramWithSource(context, 1, 
             (const char **)&source_str, (const size_t *)&source_size, &ret);
 
     // Build the program
-    ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
+    CHECK_CL_ERR(ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL));
 
     // Create the OpenCL kernel
-    cl_kernel kernel = clCreateKernel(program, "vector_add", &ret);
+    printf("Creating kernel from %s\n", KERNEL);
+    CHECK_CL_ERR(cl_kernel kernel = clCreateKernel(program, KERNEL, &ret));
 
     // Set the arguments of the kernel
-    ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&a_mem_obj);
-    ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&b_mem_obj);
-    ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&c_mem_obj);
+    CHECK_CL_ERR(ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&a_mem_obj));
+    CHECK_CL_ERR(ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&b_mem_obj));
+    CHECK_CL_ERR(ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&c_mem_obj));
     
     // Execute the OpenCL kernel on the list
     size_t global_item_size = LIST_SIZE; // Process the entire lists
     size_t local_item_size = 64; // Process in groups of 64
     cl_event event;
-    ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, 
-            &global_item_size, &local_item_size, 0, NULL, &event);
+    CHECK_CL_ERR(ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, 
+            &global_item_size, &local_item_size, 0, NULL, &event));
 
     clWaitForEvents(1, &event);
     clFinish(command_queue);
@@ -119,13 +156,13 @@ char platform_name[128] = {0};
     clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_START, sizeof(time_start), &time_start, NULL);
     clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_END, sizeof(time_end), &time_end, NULL);
 
-    double nanoSeconds = time_end-time_start;
-    printf("OpenCl device %u of %u platforms. Execution time is: %0.3f milliseconds \n", ret_num_devices, ret_num_platforms, nanoSeconds / 1000000.0);
+    double nanoSeconds = time_end - time_start;
+    printf("OpenCl device %u of %u platforms. Execution time is: %0.3f milliseconds (%f ns) \n", ret_num_devices, ret_num_platforms, nanoSeconds / 1000000.0, nanoSeconds);
 
     // Read the memory buffer C on the device to the local variable C
-    int *C = (int*)malloc(sizeof(int)*LIST_SIZE);
+    TYPE *C = (TYPE*)malloc(sizeof(TYPE)*LIST_SIZE);
     ret = clEnqueueReadBuffer(command_queue, c_mem_obj, CL_TRUE, 0, 
-            LIST_SIZE * sizeof(int), C, 0, NULL, NULL);
+            LIST_SIZE * sizeof(TYPE), C, 0, NULL, NULL);
 
     // Display the result to the screen
 //     for(i = 0; i < LIST_SIZE; i++)
--- OpenCLGS/vector_add_kernel.cl
+++ OpenCLGS/vector_add_kernel.cl
@@ -3,7 +3,41 @@ __kernel void vector_add(__global int *A, __global int *B, __global int *C) {
     // Get the index of the current element
     int i = get_global_id(0);
 
+    int c = 0;
+
     // Do the operation
-    for(int j = 0; j < 1000; ++j)
-    C[i] += A[i] + B[i];
+    for(int j = 0; j < 100000; ++j)
+        c += A[i] + B[i];
+
+    C[i] = c;
+}
+
+
+__kernel void vector_add_double(__global double *A, __global double *B, __global double *C) {
+    
+    // Get the index of the current element
+    int i = get_global_id(0);
+
+    double c = 0.0;
+
+    // Do the operation
+    for(int j = 0; j < 10000; ++j)
+        c += A[i] + B[i];
+
+    C[i] = c;
+}
+
+
+__kernel void vector_add_float(__global float *A, __global float *B, __global float *C) {
+    
+    // Get the index of the current element
+    int i = get_global_id(0);
+
+    float c = 0.0;
+
+    // Do the operation
+    for(int j = 0; j < 100000; ++j)
+        c += A[i] + B[i];
+
+    C[i] = c;
 }
